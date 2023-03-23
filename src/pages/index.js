e5e7bcf0f72ccb9af79bc8f3c22e75670560bd06
Laura Pham <l.pham2311@gmail.com>
2023-03-23 22:33:29 +1100

    poultryline template app

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,172 +1,249 @@
 import * as React from "react"
+import {useState, useEffect} from 'react'
 
 const pageStyles = {
   color: "#232129",
-  padding: 96,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center'
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
+const contentStyle = {
+  display: 'flex',
+   gap: '12px', 
+   flexDirection: 'column', 
+   maxWidth: '650px',
+   width: '-webkit-fill-available',
+   fontSize: '18px'
+  }
+
+const tableStyle = {
+  borderSpacing: '8px',
+  backgroundColor: 'white',
+  border: '1px solid',
+  borderRadius: '8px'
 }
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
+const inputStyle = {
+  borderRadius: '4px',
+  border: '1px solid black',
+  padding: '4px',
+  fontSize: '24px',
+  width: '50px',
 }
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
+const actionButtonStyle = {
+  display: 'flex',
+  justifyContent: 'space-between'
 }
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
+const buttonStyle = {
+  width: 'fit-content',
+  backgroundColor: 'green',
+  border: 'none',
+  borderRadius: '8px',
+  fontSize: '18px',
+  padding: '8px',
+  color: 'white',
+  cursor: 'pointer',
+}
+const copyButtonStyle = {
+  ...buttonStyle,
+  backgroundColor: 'green',
 }
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+const clearButtonStyle = {
+  ...buttonStyle,
+  backgroundColor: 'gray',
 }
 
-const links = [
+const textareaStyle = {
+  resize: 'none',
+  overflow: 'hidden',
+  fontFamily: 'inherit',
+  fontSize: '18px',
+  borderRadius: '8px',
+  padding: '8px',
+}
+const itemList = [
   {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
+    metric: 'kg',
+    name: 'Giblet'
   },
   {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
+    metric: 'kg',
+    name: 'Liver'
   },
   {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
+    metric: 'kg',
+    name: 'Hearts'
   },
   {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
+    metric: 'box',
+    name: 'Necks'
   },
   {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
+    metric: 'boxes',
+    name: 'WINGS XXLarge'
   },
   {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
+    metric: 'boxes',
+    name: 'Supreme XL FRESH & INGHAM BRAND no steggles l/s'
+  },
+  {
+    metric: 'boxes',
+    name: 'Inghams size 9 to 11'
+  },
+  {
+    metric: 'boxes',
+    name: 'Baiada size 17 birds'
+  },
+  {
+    metric: 'boxes',
+    name: ' Baiada size 21 to 23 bird'
+  },
+  {
+    metric: 'boxes',
+    name: 'Baiada size 26 to 32 bird big size l/s'
+  },
+  {
+    metric: 'kg',
+    name: 'Maryland fillet skin off'
+  },
+  {
+    metric: 'kg',
+    name: 'Maryland fillet skin on'
+  },
+  {
+    metric: 'kg',
+    name: 'Drumstick fillet off'
+  },
+  {
+    metric: 'kg',
+    name: 'Spare'
+  },
+  {
+    metric: 'boxes',
+    name: 'Ribs skin on'
+  },
+  {
+    metric: 'boxes',
+    name: 'Chest bone'
+  },
+  {
+    metric: 'kg',
+    name: 'Breast fillet skinless'
+  },
+  {
+    metric: 'kg',
+    name: 'Breast fillet skin on'
+  },
+  {
+    metric: 'kg',
+    name: 'Spare ribs skin off'
   },
 ]
 
+const metricWithSpace = ['box', 'boxes']
+
+const metricToString = (metric) => {
+  if (metricWithSpace.includes(metric)) {
+    return ` ${metric}`
+  }
+  return `${metric}`
+}
+
+const activeValue = (value) => {
+  return !(!value || value <= '0')
+}
+
+
 const IndexPage = () => {
+  const [templateText, setTemplateText] = useState("")
+  const [isCopyClicked, setIsCopyClicked] = useState(false)
+  console.log({isCopyClicked})
+
+
+  const handleClickCopy = () => {
+    let textarea = document.querySelector('textarea')
+    textarea.select()
+    document.execCommand('copy');
+    setIsCopyClicked(true)
+  }
+
+  useEffect(() => {
+    if (isCopyClicked) {
+      setTimeout(() => {
+        setIsCopyClicked(false)
+      }, 2500)
+    }
+  }, [isCopyClicked])
+
+  const handleOnSubmit = () => {
+    const itemRows = document.querySelectorAll('tr')
+    let existingItems = []
+    itemRows.forEach((item, index) => {
+      if (index === 0) return
+      const inputElement = item.querySelector('input[name="item-amount"]')
+      if (!activeValue(inputElement.value)) return
+      const metricElement = item.querySelector('td[name="item-metric"]')
+      const nameElement = item.querySelector('td[name="item-name"]')
+      const itemRowString = `${inputElement.value}${metricToString(metricElement.innerHTML)} ${nameElement.innerHTML}`
+      existingItems.push(itemRowString)
+    })
+    if (existingItems.length === 0) return
+    const orderString = existingItems.join("\n")
+    // setTemplateText 
+    setTemplateText(orderString)
+  }
+
+  const handleInputBlur = ( e )  => {
+    handleOnSubmit()
+    const inputRow = e.target.parentElement
+
+    // Check if active input
+    if (!activeValue(e.target.value)) {
+      inputRow.style.color = 'inherit'
+      inputRow.style.border = '1px solid red'
+    } else {
+      // add text color to active row
+      inputRow.style.color = 'green'
+      inputRow.style.border = 'none'
+
+    }
+  }
+
+  const handleClickClear = () => {
+    window.location.reload();
+  }
+
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <h1>PoultryLine Order Template 🐔</h1>
+      <div style={contentStyle}>
+     <table style={tableStyle}>
+        <tr>
+          <th>Amount</th>
+          <th>Metric</th>
+          <th>Item Name</th>
+        </tr>
+        {itemList.map((item, index) => {
+          return(
+          <tr>
+          <input type="number" min="0" id={index} name="item-amount" onChange={handleInputBlur} style={inputStyle}/>
+          <td name="item-metric">{item.metric}</td>
+          <td name="item-name">{item.name}</td>
+        </tr>)
+        })}
+      </table>
+    <div style={actionButtonStyle}>
+      <button onClick={handleClickCopy} style={copyButtonStyle}>{isCopyClicked ? 'copied!' : 'copy'}</button>
+      <button onClick={handleClickClear} style={clearButtonStyle}>clear</button>
+    </div>
+      <textarea id="template-text" name="template-text" rows={itemList.length} cols="50" value={templateText} contenteditable='false' style={textareaStyle}/>
+      </div>
+      <p>Made with ❤️ by Laura Pham</p>
     </main>
   )
 }
